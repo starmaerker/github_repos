70e72509dbb8b664a058384435626ffb7293f043
Andreas Gropp <starmaerker@googlemail.com>
2020-04-26 15:02:58 +0200

    add links to repo

--- search.py
+++ search.py
@@ -3,11 +3,11 @@ import requests
 
 def search_repos(lang):
     result = requests.get(f'https://api.github.com/search/repositories?q=language:{lang}&sort=stars&order=desc').json()
-
     counter = 0
     repo_list = {}
     while counter < 30:
         repo = result['items'][counter]
-        repo_list[counter] = [repo['name'], repo['description'], repo['stargazers_count'], repo['owner']['avatar_url']]
+        repo_list[counter] = [repo['name'], repo['description'], repo['stargazers_count'], repo['owner']['avatar_url'], repo['owner']['html_url']]
         counter += 1
     return repo_list
+
