299388609226c3fa405890e801aebca6a920f625
Andreas Gropp <starmaerker@googlemail.com>
2020-05-12 20:50:21 +0200

    add model.py

---
+++ model.py
@@ -0,0 +1,10 @@
+from app import db
+
+
+class Lang(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    stargazers = db.Column(db.BigInteger, index=True)
+
+    def __repr__(self):
+        return '<Lang {}>'.format(self.name)
