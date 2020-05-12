9e660f907bfa00ae2e8745913cb9f494bf42dd20
Andreas Gropp <starmaerker@googlemail.com>
2020-05-12 21:58:03 +0200

    add timestamp field to model

--- model.py
+++ model.py
@@ -1,11 +1,12 @@
 from app import db
+from datetime import datetime
 
 
 class Lang(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     stargazers = db.Column(db.BigInteger, index=True)
-
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
     def __repr__(self):
         return '<Lang {}>'.format(self.name)
